52f04b8b8b601fd152ad51365bf0efb69e59bd96
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-01-18 15:04:02 +0530

    Odd Even program done

---
+++ PracticeProblem.java
@@ -0,0 +1,8 @@
+class PracticeProblem{
+	public static void main(String args[]){
+		for(int i=1;i<=10;i++){
+			if(i%2==0) System.out.println(i+" is Even number");
+			else System.out.println(i+" is Odd number");
+		}
+	}
+}
